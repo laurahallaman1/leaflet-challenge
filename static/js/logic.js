5b21418d4ed5b55194b6862f2ad344e6ec159c6e
laurahallaman1 <56806860+laurahallaman1@users.noreply.github.com>
2020-04-25 14:30:02 -0400

    fixed legend
    legend updated

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,10 +1,6 @@
 // Store API link
 var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-//var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-01-01&endtime=" +
-//  "2014-04-01&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337"
-
-
 function markerSize(mag) {
   return mag * 25000;
 }
@@ -99,6 +95,6 @@ function createMap(earthquakes) {
   }).addTo(myMap);
 
 // Add Legend 
-map.legendControl.addLegend(document.getElementById('legend').innerHTML);
+// Added via HTML
 
 }
